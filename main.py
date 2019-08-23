640bea08295c5cbf9eeb996aa8dfad55d4e2e9e2
Ilia <politovi@yandex-team.ru>
2019-08-23 12:08:52 +0300

    New config

--- main.py
+++ main.py
@@ -4,30 +4,38 @@ import socket
 
 app = Flask(__name__)
 
+fixed_latitude = 0.0
+fixed_longitude = 0.0
+fixed_height = 0
+rtk_port = 11111
 
 @app.route('/')  # opening the url http://*port_number*/ will call the data_print function:
 def data_print():
-    conf = _config_data_print()
+    # conf = _config_data_print()
+    conf = 'Config data: <br/> <br/>Latitude: {} <br/>Longitude: {} <br/>Height: {}'.\
+      format(fixed_latitude, fixed_longitude, fixed_height)
     port = _port_data_print()
     return conf + '<br/>'*2 + port
 
 
 #  getting data from config:
-def _config_data_print():
+def _config_data_read():
     with open('config.txt', 'r', encoding='utf-8') as f:
         text = f.read()
-        space = re.compile('[ ]*')  # regexp for one or more spaces to separate numbers form text file
+        space = re.compile('\n')  # regexp for one or more spaces to separate numbers form text file
 
-        config_list = space.split(text)[2:5]  # 2:5 are the only numbers we need (0 - date, 1 - time)
-        result = 'Config data: <br/> <br/>Latitude: {} <br/>Longitude: {} <br/>Height: {}'.\
-            format(config_list[0], config_list[1], config_list[2])  # gonna fix that a lil bit later
-    return result
+        config_list = space.split(text)[0:3]  # 2:5 are the only numbers we need (0 - date, 1 - time)
+        fixed_latitude = config_list[0]
+        fixed_longitude = config_list[1]
+        fixed_height = config_list[2]
+        rtk_port = config_list[3]
+    return true
 
 
 # getting data from port:
 def _port_data_print():
     host = socket.gethostname()
-    port = 11111
+    port = rtk_port
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # for ipv4 and reliable thread socket respectively
     s.connect((host, port))
     data = s.recv(1024)  # receiving up to 1024 bytes from our socket
@@ -41,4 +49,6 @@ def _port_data_print():
 
 
 if __name__ == '__main__':
+    if (_config_data_read() != true)
+      print('Error reading config')
     app.run()
